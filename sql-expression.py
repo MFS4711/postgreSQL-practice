877af618f08dac95c4fe0b8299e4b2cc597ef3c8
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-21 14:28:25 +0000

    add neq queries using sqlalchemy

--- sql-expression.py
+++ sql-expression.py
@@ -43,7 +43,24 @@ track_table = Table(
 # making the connection
 with db.connect() as connection:
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Seem like with_only_columns() - contents are ["table variable".c."column name"] - gives all column contents
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Seems like .where() - contents are ("table variable".c."column name" == "column value") - gives a specific
+    # Query 3 = select only "Queen" from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 - select only by "ArtistId" #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - select only the albums with the "ArtistId" #51 on the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+    # select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
