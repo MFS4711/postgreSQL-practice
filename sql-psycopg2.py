b3e6264040e003df7b250b265029beadae71e621
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-21 12:04:43 +0000

    test custom query

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -20,10 +20,13 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
 # Query 5 - select only the albums with the "ArtistId" #51 on the "Album" table
-cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - select all tracks where the Composer is "Black Eyed Peas" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Metallica"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
